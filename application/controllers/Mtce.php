1b72bfc337e02d39ab76a3e34103fd166c0e328e
Daniel Tian <daniel_tian2002@hotmail.com>
2017-03-02 16:10:33 -0800

    modified Mtce controller to delete irrelevant comments

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -5,27 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Mtce extends Application
 {
 
-	/*public function index()
-	{
-		$this->data['pagetitle'] = 'TODO List Maintenance';
-        $tasks = $this->tasks->all(); // get all the tasks
-
-        // substitute the status name
-        foreach ($tasks as $task)
-                if (!empty($task->status))
-                        $task->status = $this->statuses->get($task->status)->name;
-
-        // convert the array of task objects into an array of associative objects       
-        foreach ($tasks as $task)
-                $converted[] = (array) $task;
-
-        // and then pass them on
-        $this->data['display_tasks'] = $converted;
-        $this->data['pagebody'] = 'itemlist';
-        $this->render();
-	}*/
-
-
 	public function index()
 	{
 		$this->data['pagetitle'] = 'TODO List Maintenance';
